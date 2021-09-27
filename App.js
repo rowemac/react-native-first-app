74ad8d5b5a8ca4cc546b221e8ee9c8a894cd6f7f
Mac Rowe <m.rowe42@gmail.com>
2021-09-27 16:49:19 -0400

    Begin building a course goals to do list for iOS and Android

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change text" onPress={ () => setOutputText('The text changed!')} />
-      <StatusBar style="auto" />
+    <View style={{padding: 50}}>
+      <View>
+        <TextInput placeholder="Course Goal" style={{ borderColor: 'black', borderWidth: 1, padding: 10 }} />
+        <Button title="ADD" />
+        <View>
+
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
