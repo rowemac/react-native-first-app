90c0147cb6a4ab9bc0918a0a9fbd198114f6b492
Mac Rowe <m.rowe42@gmail.com>
2021-09-27 17:05:21 -0400

    Apply inline styling first

--- App.js
+++ App.js
@@ -5,8 +5,11 @@ import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={{padding: 50}}>
-      <View>
-        <TextInput placeholder="Course Goal" style={{ borderColor: 'black', borderWidth: 1, padding: 10 }} />
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <TextInput 
+          placeholder="Course Goal" 
+          style={{ width: 200, borderColor: 'black', borderWidth: 1, padding: 10 }} 
+        />
         <Button title="ADD" />
         <View>
 
