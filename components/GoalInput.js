5bac7fc7abfc2e9a622d97b24863332c9e156b2f
Mac Rowe <m.rowe42@gmail.com>
2021-09-28 12:05:33 -0400

    Fix button styling for Android

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -8,17 +8,25 @@ const GoalInput = props => {
         setEnteredGoal(enteredText)
     };
 
+    const addGoalHandler = () => {
+        props.onAddGoal(enteredGoal); 
+        setEnteredGoal('');
+    }; 
+
     return (
         <Modal visible={props.visible} animationType="slide">
             <View style={styles.inputContainer}>
-            <TextInput 
-            placeholder="Course Goal" 
-            style={styles.input} 
-            onChangeText={goalInputHandler}
-            value={enteredGoal}
-            />
-            <Button title="ADD" onPress={() => props.onAddGoal(enteredGoal)} />
-        </View>
+                <TextInput 
+                placeholder="Course Goal" 
+                style={styles.input} 
+                onChangeText={goalInputHandler}
+                value={enteredGoal}
+                />
+                <View style={styles.buttonContainer}>
+                    <Button title="CANCEL" color='red' onPress={props.onCancel}/>
+                    <Button title="ADD" onPress={addGoalHandler} />
+                </View>
+            </View>
       </Modal>
     )
 }; 
@@ -29,12 +37,19 @@ const styles = StyleSheet.create({
         justifyContent: 'center', 
         alignItems: 'center'
     },
-        input: {
+    
+    input: {
         width: '80%',
         borderColor: 'black',
         borderWidth: 1, 
         padding: 10,
         marginBottom: 10
+    },
+
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '60%'
     }
 });
 
