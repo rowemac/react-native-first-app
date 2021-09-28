c72fbd575cd4bc6624f2ca25b88f0b7e40fbba20
Mac Rowe <m.rowe42@gmail.com>
2021-09-28 10:22:51 -0400

    Add FlatList component to take the place of ScrollView

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
    const [enteredGoal, setEnteredGoal] = useState('');
@@ -11,7 +11,8 @@ export default function App() {
     };
 
     const addGoalHandler = () => {
-      setCourseGoals(currentGoals => [...currentGoals, enteredGoal]); 
+      setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: enteredGoal }
+      ]); 
     };
 
   return (
@@ -25,19 +26,22 @@ export default function App() {
         />
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <ScrollView>
-        {courseGoals.map(goal => (<View  key={goal} style={styles.listItem}>
-            <Text>{goal}</Text>
+      <FlatList 
+        keyExtractor={(item, index) => item.id }
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
           </View>
-        ))}
-      </ScrollView>
+        )} 
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50
+    padding: 70
   }, 
   inputContainer: {
     flexDirection: 'row',
